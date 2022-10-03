5f555b4f954d5df80e8f5d7ae0a651757b5ce13a
juba0x00 <mahmoudhawash90@gmail.com>
2022-10-03 17:29:56 +0200

    setup.sh improvements: 	adding nuclei installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-pip install requests
-pip install colorama
-pip install json
-apt-get install dirsearch
-apt-get install nuclei
+pip3 install -r requirements.txt
+apt install dirsearch wafw00f -y
+git clone https://github.com/projectdiscovery/nuclei.git 
+cd nuclei/v2/cmd/nuclei/
+go build .
+cp nuclei /usr/local/bin
